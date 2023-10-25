5f4b7ed1f45ea2133212db5470d90e1fb2d602b6
Monalika Patnaik <monalikapatnaik114@gmail.com>
2023-10-26 02:57:02 +0530

    changes

--- extension.js
+++ extension.js
@@ -2,51 +2,50 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const axios = require('axios');
-const OPENAI_API_KEY = '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'; // Replace with your OpenAI API key
-const OPENAI_API_ENDPOINT = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+require("dotenv").config();
+
+const apiKey = process.env.OPENAI_API_KEY;
+
+const client = axios.create({
+  headers: {
+    Authorization: "Bearer " + apiKey,
+  },
+});
+console.log('API Key:', apiKey);
+// const { OpenAIAPI } = require('openai');
+// const openai = new OpenAIAPI({
+//     key: OPENAI_API_KEY,
+//     model: 'text-davinci-003'
+// });
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+// /**
+//  * @param {vscode.ExtensionContext} context
+//  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "auto-code-documentation" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('auto-code-documentation.extension', function () {
+	let disposable = vscode.commands.registerCommand('auto-code-documentation.extension', () =>{
 		const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const document = editor.document;
-            const text = document.getText();
+    if (editor) {
+        const selection = editor.selection;
+        const selectedCode = editor.document.getText(selection);
 
-            try {
-                const response = axios.post(OPENAI_API_ENDPOINT, {
-                    prompt: text,
-                    max_tokens: 150, // You can adjust this value based on your requirements
-                }, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${OPENAI_API_KEY}`
-                    }
-                });
+        // Logic to generate documentation from selectedCode
+        const documentation = generateDocumentation(selectedCode);
 
-                const generatedDocumentation = response.data.choices[0].text;
-                
-                const outputChannel = vscode.window.createOutputChannel('Generated Documentation');
-				outputChannel.appendLine(generatedDocumentation);
-                outputChannel.show();
-            } catch (error) {
-                console.error('Error generating documentation:', error);
-                vscode.window.showErrorMessage('Error generating documentation. Check the console for details.');
-            }
-		}
+        // Display the generated documentation in a new editor tab
+        vscode.workspace.openTextDocument({ content: documentation, language: 'markdown' }).then(document => {
+            vscode.window.showTextDocument(document, vscode.ViewColumn.Beside);
+        });
+    }
+    else {
+        vscode.window.showErrorMessage('No code selected!');
+    }
 		vscode.window.showInformationMessage('Hello World from Auto Code Documentation!');
 	});
 
@@ -59,4 +58,21 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
+}
+async function generateDocumentation(selectedCode) {
+    const params= {
+         prompt:`Generate documentation for the following code:\n\n${selectedCode}\n\nDocumentation:`,
+            model: "text-davinci-003",
+            max_tokens: 2048,
+            temperature: 1,
+        };
+        try {
+          const response = await client.post("https://api.openai.com/v1/completions", params);
+          console.log('OpenAI API Response:', response);
+          return response.data.choices[0].text;
+      } catch (error) {
+          console.error('Error generating documentation:', error);
+          return 'Error generating documentation.';
+      }
+
 }
